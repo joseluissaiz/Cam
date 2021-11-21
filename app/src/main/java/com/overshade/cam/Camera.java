1fa4335dcda236ccef19d558dc07e110ec9ebfb7
Jose Luis Saiz Lopez <joselu609@gmail.com>
2021-11-21 17:14:16 +0100

    Added flashlight for back and front camera.
    + Functions improved and encapsulated.
    + Not start camera again on enable or disable flashlight.
    
    -> TODO NEXT: Zoom option

--- app/src/main/java/com/overshade/cam/Camera.java
+++ app/src/main/java/com/overshade/cam/Camera.java
@@ -1,6 +1,9 @@
 package com.overshade.cam;
 
+import static java.lang.Thread.sleep;
+
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -16,6 +19,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.util.Rational;
 import android.util.Size;
@@ -24,21 +28,30 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.FlashMode;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageAnalysisConfig;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureConfig;
 import androidx.camera.core.Preview;
 import androidx.camera.core.PreviewConfig;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.exifinterface.media.ExifInterface;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.overshade.cam.WidgetAnimations.ButtonRotateAnimation;
 import com.overshade.cam.WidgetAnimations.ButtonScaleAnimation;
 
@@ -51,6 +64,7 @@ import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -60,9 +74,21 @@ public class Camera extends Fragment {
     TextureView          txView;
     AppCompatImageButton photoButton;
     AppCompatImageButton flipButton;
+    AppCompatImageButton flashButton;
     AppCompatImageButton galleryButton;
     //Camera config
-    CameraX.LensFacing   lensFacing = CameraX.LensFacing.FRONT;
+    Preview              preview;
+    ImageCapture         imgCap;
+    ImageCaptureConfig   imgCapConfig;
+    ImageAnalysis        analysis;
+    Size                 screen;
+    Rational             asp;
+    FlashMode            flMode;
+    CameraX.LensFacing   lensFacing;
+    boolean              torchMode;
+    //Screen fx
+    ConstraintLayout     cameraTook;
+    ConstraintLayout     frontFlash;
 
 
     /* Constructor */
@@ -80,8 +106,28 @@ public class Camera extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container,
                              Bundle savedInstanceState) {
+        // setting fragment in fullscreen
+        requireActivity().getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        int currentApiVersion = Build.VERSION.SDK_INT;
+        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+            requireActivity().getWindow().getDecorView()
+                    .setSystemUiVisibility(flags);
+            final View decorView = getActivity().getWindow().getDecorView();
+            decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
+                if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                    decorView.setSystemUiVisibility(flags);
+                }
+            });
+        }
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_camera, container,
                 false);
@@ -89,25 +135,64 @@ public class Camera extends Fragment {
         txView = view.findViewById(R.id.view_finder);
         photoButton = view.findViewById(R.id.capture_button);
         flipButton = view.findViewById(R.id.flip_button);
+        flashButton = view.findViewById(R.id.flash_button);
 
         flipButton.setOnClickListener(v -> {
             new ButtonRotateAnimation
                     (flipButton, 180f, 100).start();
             if (lensFacing == CameraX.LensFacing.FRONT) {
                 lensFacing = CameraX.LensFacing.BACK;
+                if (torchMode) {
+                    // if the torch is on, turn the camera flash on
+                    setCameraFlash(true);
+                } else {
+                    setCameraFlash(false);
+                }
             } else {
                 lensFacing = CameraX.LensFacing.FRONT;
+                if (torchMode) {
+                    // if the torch is on, turn camera flash off
+                    flMode = FlashMode.OFF;
+                    //And enable the front flash
+                } else {
+                    flMode = FlashMode.OFF;
+                }
             }
             startCamera();
         });
 
+        flashButton.setOnClickListener(view1 -> {
+            torchMode = !torchMode;
+            flashButton.setBackgroundResource((torchMode) ?
+                    R.drawable.ic_flash_on : R.drawable.ic_flash_off);
+            if (lensFacing == CameraX.LensFacing.BACK) {
+                if (torchMode) {
+                    setCameraFlash(true);
+                } else {
+                    setCameraFlash(false);
+                }
+            } else {
+                flMode = FlashMode.OFF;
+            }
+
+        });
+
+
         galleryButton = view.findViewById(R.id.gallery_button);
 
+        cameraTook = view.findViewById(R.id.camera_took);
+        frontFlash = view.findViewById(R.id.frontal_flash);
+
+        //Camera presets config
+        lensFacing = CameraX.LensFacing.BACK;
+        torchMode = false;
+        flMode = FlashMode.OFF;
+
+        //Start camera
         startCamera();
         loadGalleryButton();
         return view;
     }
-
 
     /* Camera methods */
 
@@ -120,8 +205,8 @@ public class Camera extends Fragment {
         int aspRatioW = txView.getWidth(); //get width of screen
         int aspRatioH = txView.getHeight(); //get height
 
-        Rational asp = aspectRatio(aspRatioW, aspRatioH); //aspect ratio
-        Size screen = new Size(aspRatioW, aspRatioH); //size of the screen
+        asp = aspectRatio(aspRatioW, aspRatioH); //aspect ratio
+        screen = new Size(aspRatioW, aspRatioH); //size of the screen
 
         //Config obj for preview/viewfinder thingy.
         PreviewConfig pConfig = new PreviewConfig.Builder()
@@ -131,7 +216,7 @@ public class Camera extends Fragment {
                 .setLensFacing(lensFacing)
                 .setTargetResolution(screen)
                 .build();
-        Preview preview = new Preview(pConfig); //lets build it
+        preview = new Preview(pConfig); //lets build it
 
         //To update the surface texture we have to destroy it first, then
         // re-add it
@@ -149,62 +234,27 @@ public class Camera extends Fragment {
         /* Image capture */
 
         //Config obj, selected capture mode
-        ImageCaptureConfig imgCapConfig = new ImageCaptureConfig.Builder()
+        imgCapConfig = new ImageCaptureConfig.Builder()
                 .setLensFacing(lensFacing)
+                .setFlashMode(flMode)
                 .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
                 .setTargetRotation(requireActivity().getWindowManager()
                         .getDefaultDisplay().getRotation())
                 .setTargetAspectRatio(asp)
                 .setTargetResolution(screen)
                 .build();
-        final ImageCapture imgCap = new ImageCapture(imgCapConfig);
+
+        imgCap = new ImageCapture(imgCapConfig);
 
         photoButton.setOnClickListener(v -> {
             new ButtonScaleAnimation(photoButton,
                     100, 100).start();
-
-            SimpleDateFormat sdf =
-                    new SimpleDateFormat("yyyyMMdd_HHmmss",
-                            Locale.getDefault());
-            String fileName = sdf.format(new Date());
-            String filePath = Environment.getExternalStorageDirectory() +
-                    File.separator + "Cam" + File.separator + fileName +
-                    ".jpg";
-            File file = new File(filePath);
-
-            imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
-                @Override
-                public void onImageSaved(@NonNull File file) {
-                    String msg = "Photo capture succeeded: "
-                            + file.getAbsolutePath();
-                    Toast.makeText(requireActivity().getBaseContext(), msg,
-                            Toast.LENGTH_LONG).show();
-                    if (lensFacing == CameraX.LensFacing.FRONT) {
-                        flipImage(file);
-                    }
-
-                    //Updating android files database
-                    Uri imageUri = Uri.parse("file://" + filePath);
-                    Intent mediaScanIntent =
-                            new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-                                    imageUri);
-                    mediaScanIntent.setData(Uri.fromFile(file));
-                    requireActivity().sendBroadcast(mediaScanIntent);
-                    loadGalleryButton();
-                }
-
-                @Override
-                public void onError(
-                        @NonNull ImageCapture.UseCaseError useCaseError,
-                        @NonNull String message, @Nullable Throwable cause) {
-                    String msg = "Photo capture failed: " + message;
-                    Toast.makeText(requireActivity().getBaseContext(), msg,
-                            Toast.LENGTH_LONG).show();
-                    if (cause != null) {
-                        cause.printStackTrace();
-                    }
-                }
-            });
+            if (torchMode && lensFacing == CameraX.LensFacing.FRONT) {
+                //Set the flash countdown and take photo
+                takeFrontFlashPhoto();
+            } else {
+                takePhoto();
+            }
         });
 
 
@@ -215,7 +265,7 @@ public class Camera extends Fragment {
                         (ImageAnalysis.ImageReaderMode.ACQUIRE_LATEST_IMAGE)
                 .setLensFacing(lensFacing)
                 .build();
-        ImageAnalysis analysis = new ImageAnalysis(imgAConfig);
+        analysis = new ImageAnalysis(imgAConfig);
         analysis.setAnalyzer(
                 (image, rotationDegrees) -> {
                     //y'all can add code to analyse stuff here idek go wild.
@@ -223,6 +273,57 @@ public class Camera extends Fragment {
 
         //bind to lifecycle:
         CameraX.bindToLifecycle(this, analysis, imgCap, preview);
+    }
+
+    private void takePhoto() {
+        SimpleDateFormat sdf =
+                new SimpleDateFormat("yyyyMMdd_HHmmss",
+                        Locale.getDefault());
+        String fileName = sdf.format(new Date());
+        String filePath = Environment.getExternalStorageDirectory() +
+                File.separator + "Cam" + File.separator + fileName +
+                ".jpg";
+        File file = new File(filePath);
+
+        imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
+            @Override
+            public void onImageSaved(@NonNull File file) {
+                preview.enableTorch(false);
+                makeTookPhotoFx();
+                frontFlash.setVisibility(View.GONE);
+                //String msg = "Photo capture succeeded: "
+                //        + file.getAbsolutePath();
+                //Toast.makeText(requireActivity().getBaseContext(), msg,
+                //        Toast.LENGTH_LONG).show();
+
+                //different rotation depending of lensFcing
+                if (lensFacing == CameraX.LensFacing.FRONT) {
+                    flipImage(file);
+                }
+
+                //Updating android files database
+                Uri imageUri = Uri.parse("file://" + filePath);
+                Intent mediaScanIntent =
+                        new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                                imageUri);
+                mediaScanIntent.setData(Uri.fromFile(file));
+                requireActivity().sendBroadcast(mediaScanIntent);
+                loadGalleryButton();
+            }
+
+            @Override
+            public void onError(
+                    @NonNull ImageCapture.UseCaseError useCaseError,
+                    @NonNull String message, @Nullable Throwable cause) {
+                frontFlash.setVisibility(View.GONE);
+                String msg = "Photo capture failed: " + message;
+                Toast.makeText(requireActivity().getBaseContext(), msg,
+                        Toast.LENGTH_LONG).show();
+                if (cause != null) {
+                    cause.printStackTrace();
+                }
+            }
+        });
     }
 
     private void updateTransform() {
@@ -282,7 +383,7 @@ public class Camera extends Fragment {
         // create new matrix for transformation
         Matrix matrix = new Matrix();
         matrix.postScale(-1f, 1f,
-                bm.getWidth()/2f, bm.getHeight()/2f);
+                bm.getWidth() / 2f, bm.getHeight() / 2f);
         matrix.postRotate(90f);
         // bitmap transformed
         Bitmap newBm = Bitmap.createBitmap(bm, 0, 0,
@@ -293,11 +394,11 @@ public class Camera extends Fragment {
                     new BufferedOutputStream(new FileOutputStream(file));
             newBm.compress(Bitmap.CompressFormat.JPEG, 100, os);
             os.close();
-            Toast.makeText(requireActivity(), "and flipped successfully",
-                    Toast.LENGTH_SHORT).show();
+            //Toast.makeText(requireActivity(), "and flipped successfully",
+            //        Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
-            Toast.makeText(requireActivity(), "but it cannot be flipped",
-                    Toast.LENGTH_SHORT).show();
+            //Toast.makeText(requireActivity(), "but it cannot be flipped",
+            //        Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
 
@@ -310,54 +411,69 @@ public class Camera extends Fragment {
         // start loop trough files in directory
         if (dir.exists()) {
             File[] files = dir.listFiles();
-            if (files != null) {
-                try {
-                    Bitmap bm = BitmapFactory.decodeFile(
-                            files[files.length-1].getAbsolutePath());
-                    Uri uri = Uri.fromFile(files[0]);
-                    Context ctxt = requireActivity();
-                    Bitmap newBm = rotateImageIfRequired(ctxt, bm, uri);
-                    galleryButton.setImageBitmap(newBm);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-
+            if (files != null && files.length > 0) {
+                Uri uri = Uri.fromFile(files[files.length - 1]);
+                loadImage(uri);
             }
         }
     }
 
-
-    //Rotate an image if required.
-    private static Bitmap rotateImageIfRequired(Context context, Bitmap img, Uri selectedImage) throws IOException {
-
-        InputStream input = context.getContentResolver().openInputStream(selectedImage);
-        ExifInterface ei;
-        if (Build.VERSION.SDK_INT > 23)
-            ei = new ExifInterface(input);
-        else
-            ei = new ExifInterface(selectedImage.getPath());
-
-        int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
-
-        switch (orientation) {
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                return rotateImage(img, 90);
-            case ExifInterface.ORIENTATION_ROTATE_180:
-                return rotateImage(img, 180);
-            case ExifInterface.ORIENTATION_ROTATE_270:
-                return rotateImage(img, 270);
-            default:
-                return img;
-        }
+    private void loadImage(Uri uri) {
+        Glide.with(this)
+                .asBitmap()
+                .load(uri) // image url
+                .placeholder(R.drawable.ic_image) // any placeholder to load at start
+                .error(R.drawable.ic_image)  // any image in case of error
+                .override(200, 200) // resizing
+                .transform(new CenterCrop(), new RoundedCorners(35))
+                .into(galleryButton);  // imageview object
     }
 
-    private static Bitmap rotateImage(Bitmap img, int degree) {
-        Matrix matrix = new Matrix();
-        matrix.postRotate(degree);
-        Bitmap rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true);
-        img.recycle();
-        return rotatedImg;
+    private void makeTookPhotoFx() {
+        requireActivity().runOnUiThread(() -> {
+            cameraTook.setVisibility(View.VISIBLE);
+
+            Handler h = new Handler();
+            h.postDelayed(() -> {
+                cameraTook.setVisibility(View.GONE);
+            }, 100);
+
+        });
+    }
+
+    private void setCameraFlash(boolean activated) {
+        if (activated) {
+            flMode = FlashMode.ON;
+        } else {
+            flMode = FlashMode.OFF;
+        }
+        imgCapConfig = new ImageCaptureConfig.Builder()
+                .setLensFacing(lensFacing)
+                .setFlashMode(flMode)
+                .setCaptureMode(ImageCapture.CaptureMode.MAX_QUALITY)
+                .setTargetRotation(requireActivity().getWindowManager()
+                        .getDefaultDisplay().getRotation())
+                .setTargetAspectRatio(asp)
+                .setTargetResolution(screen)
+                .build();
+        CameraX.unbind(imgCap);
+        imgCap = new ImageCapture(imgCapConfig);
+        CameraX.bindToLifecycle(this, imgCap);
+    }
+
+    private void takeFrontFlashPhoto() {
+        AlphaAnimation flashAnim = new AlphaAnimation(0f,1f);
+        flashAnim.setDuration(1500);
+        requireActivity().runOnUiThread(() -> {
+            frontFlash.setVisibility(View.VISIBLE);
+            frontFlash.startAnimation(flashAnim);
+
+            Handler h = new Handler();
+            h.postDelayed(() -> {
+                takePhoto();
+            }, 2000);
+
+        });
     }
 
 }
